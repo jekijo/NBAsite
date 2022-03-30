81e3d2eefc3678f1247bde78028629d707d707f7
jekijo <123jkjones@gmail.com>
2022-03-29 18:24:15 -0700

    added tabs

--- src/App.js
+++ src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 
 export default function App() {
   return (
-      <div class='text-center'>
-        <h1 class="border border-dark rounded">NBA Champion Analysis</h1>
+      <div>
+        <h1 class="text-light text-center border border-dark rounded-bottom bg-secondary">NBA Champion Analysis</h1>
+        <ul class="nav nav-pills nav-justified">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Scoring</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Defense</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Playmaking</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Size & Seed</a>
+          </li>
+        </ul>
       </div>
   );
 }
