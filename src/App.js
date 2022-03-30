a4a008f9bb829643c0fcc3a0e8fbfd67ed15c88f
jekijo <123jkjones@gmail.com>
2022-03-30 11:31:26 -0700

    design prep

--- src/App.js
+++ src/App.js
@@ -3,23 +3,57 @@ import './App.css';
 
 export default function App() {
   return (
-      <div>
-        <h1 class="text-light text-center border border-dark rounded-bottom bg-secondary">NBA Champion Analysis</h1>
-        <ul class="nav nav-pills nav-justified">
-          <li class="nav-item">
-            <a class="nav-link" href="#">Scoring</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Defense</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Playmaking</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Size & Seed</a>
-          </li>
-        </ul>
+      <div class='bg-dark'>
+        <h1 class="text-light text-center border border-dark rounded-bottom bg-secondary">Stat Coyote</h1>
+        <Tabs />
+        <Welcome />
+        <Footer />
       </div>
   );
 }
 
+
+
+function Tabs () {
+  return (
+    <div>
+        <ul class="nav nav-pills nav-justified">
+          <li class="nav-item">
+            <a class="nav-link active bg-secondary border border-dark" href="#">Offense</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active bg-secondary border border-dark" href="#">Defense & Rebounding</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active bg-secondary border border-dark" href="#">Size & Seed</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active bg-secondary border border-dark" href="#">Player Stat Comparison</a>
+          </li>
+        </ul>
+      </div>
+  ); 
+}
+
+
+
+function Footer () {
+  return (
+    <div>
+      <footer>
+        <p class='text-white'>foot</p>
+      </footer>
+    </div>
+  );
+}
+
+
+
+function Welcome () {
+  return (
+    <div>
+      <hr/>
+      <h4 className='text-white'>Welcome to this site</h4>
+    </div>
+  );
+}
