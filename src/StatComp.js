93679bd4a715bfb3d48e091238025b2c417ad7fd
jekijo <123jkjones@gmail.com>
2022-04-13 14:59:15 -0700

    update to stat comp tab

--- src/StatComp.js
+++ src/StatComp.js
@@ -5,11 +5,11 @@ export default function StatComp () {
       <div className='text-white bg-dark lh-lg mx-5 fs-5'>
         <hr className='bg-dark'/>
         <h2 class='fs-2'><strong>This section is not ready yet.</strong></h2>
-        <p>In the future, this tab will be the home to a player stat comparison visualizer. 
-          You will be able to input any two given players names, and it will return a side by side
-          comparison of the players stats in a graph. This section is currently being worked on,
-          and may be implemented in waves of updates. This may take a while to fully deploy so 
-          in the mean time, enjoy the other pages of analysis that also have much more on the way!
+        <p>In the future, this tab will be the home to further analysis in which individual player 
+          stats will be evaluated using k means clustering in order to test if a players position can 
+          be determined by their stats. This will be done on current NBA players, and players from 20 years
+          ago. From this data, we will be able to see if the trend towards position-less basketball is 
+          real, or more of the perception of how the game is played.
         </p>
       </div>
     );
